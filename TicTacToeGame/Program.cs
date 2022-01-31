7c225d3e45e065d41bfa4b1c04164d7b22c3c94e
Shubham Seth <shubhamsth1234@gmail.com>
2022-01-31 18:47:19 +0530

    [add] Shubham added TicTacToe program

---
+++ TicTacToeGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TicTacToeGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to TicTacToe Game");
+            
+        }
+    }
+}
---
+++ TicTacToeGame/TicTacToe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicTacToeGame
+{
+    public class TicTacToe
+    {
+        //UC1
+        char[] Board = new char[10];
+        public void gameBoard()
+        {
+            for (int i = 1; i < 9; i++)
+            {
+                Board[i] = ' ';
+            }
+        }
+    }
+}
